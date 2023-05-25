f38233094212e119ac9b792cefe571f3e09cea1e
shade2903 <nighshade2903@gmail.com>
2023-05-25 16:06:37 +0300

    add cascade for task12

--- lesson12/src/main/resources/TABLES_CHANGES.sql
+++ lesson12/src/main/resources/TABLES_CHANGES.sql
@@ -1,1 +1,19 @@
-select * from student;
+ALTER TABLE MARK DROP CONSTRAINT mark_student_id_fkey CASCADE;
+
+ALTER TABLE MARK DROP CONSTRAINT mark_subject_id_fkey CASCADE;
+
+ALTER TABLE PAYMENT DROP CONSTRAINT payment_type_id_fkey CASCADE;
+
+ALTER TABLE PAYMENT DROP CONSTRAINT payment_student_id_fkey CASCADE;
+
+ALTER TABLE MARK ADD CONSTRAINT mark_student_id_fkey FOREIGN KEY (student_id)
+REFERENCES student(id) ON DELETE CASCADE;
+
+ALTER TABLE MARK ADD CONSTRAINT mark_subject_id_fkey FOREIGN KEY (subject_id)
+REFERENCES subject(id) ON DELETE CASCADE;
+
+ALTER TABLE PAYMENT ADD CONSTRAINT payment_type_id_fkey FOREIGN KEY (type_id)
+REFERENCES paymenttype(id) ON DELETE CASCADE;
+
+ALTER TABLE PAYMENT ADD CONSTRAINT payment_student_id_fkey FOREIGN KEY (student_id)
+REFERENCES student(id) ON DELETE CASCADE;
