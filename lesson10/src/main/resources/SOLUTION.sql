7a4018bd2c725f35a584de0502f8d688400058cc
shade2903 <nighshade2903@gmail.com>
2023-05-25 16:11:51 +0300

    refactoring  task10

--- lesson10/src/main/resources/SOLUTION.sql
+++ lesson10/src/main/resources/SOLUTION.sql
@@ -1,10 +1,10 @@
-SELECT s.*
+SELECT s.id, s.name, s.birthday, s.groupnumber
 FROM SUBJECT AS s
          JOIN MARK AS m ON s.id = m.subject_id
 GROUP BY (s.id)
 HAVING Avg(m.mark) > (SELECT Avg(mark) FROM mark);
 
-SELECT s.*
+SELECT s.id, s.name, s.birthday, s.groupnumber
 FROM STUDENT AS s
          JOIN PAYMENT AS p ON s.id = p.student_id
 GROUP BY (s.id)
